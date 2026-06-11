1a847fbfb89ea360d751d57a2248ca94beba86e6
Henrique <henriquetrindadepereira@gmail.com>
2026-06-11 20:29:45 -0300

    configurando controller
    ajustando

--- ProjetoSupermercado/src/controller/CategoriaController.java
+++ ProjetoSupermercado/src/controller/CategoriaController.java
@@ -1,6 +1,22 @@
 package controller;
 
+import dao.CategoriaDAO;
+import java.util.List;
+import model.Categoria;
 
 public class CategoriaController {
-    
+
+    private CategoriaDAO categoriaDAO = new CategoriaDAO();
+
+    public List<Categoria> obterCategorias() {
+        return categoriaDAO.listar();
+    }
+
+    public void salvarCategoria(String nome) {
+
+        Categoria c = new Categoria();
+        c.setNome(nome);
+
+        categoriaDAO.salvar(c);
+    }
 }
