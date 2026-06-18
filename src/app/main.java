171c59785fb0c503afb021ae726e9a06dc3cef17
LucasColvero <144968002+AlunoLucasGC@users.noreply.github.com>
2026-06-17 21:55:18 -0300

    Atualizando classes

--- src/app/main.java
+++ src/app/main.java
@@ -5,6 +5,10 @@ import dao.ClienteDAO;
 import java.util.Scanner;
 import model.Categoria;
 import model.Cliente;
+import dao.ContasPagarDAO;
+import model.ContasPagar;
+import dao.ContasReceberDAO;
+import model.ContasReceber;
 
 public class main {
 
@@ -14,16 +18,18 @@ public class main {
 
         CategoriaDAO categoriaDAO = new CategoriaDAO();
         ClienteDAO clienteDAO = new ClienteDAO();
+        ContasPagarDAO contasPagarDAO = new ContasPagarDAO();
+        ContasReceberDAO contasReceberDAO = new ContasReceberDAO();
 
         int opPrincipal;
 
         do {
 
-            System.out.println("\n=== MENU PRINCIPAL ===");
             System.out.println("1 - Categoria");
             System.out.println("2 - Cliente");
+            System.out.println("3 - Contas a Pagar");
+            System.out.println("4 - Contas a Receber");
             System.out.println("0 - Sair");
-            System.out.print("Opção: ");
 
             opPrincipal = sc.nextInt();
             sc.nextLine();
@@ -51,8 +57,8 @@ public class main {
 
                             case 1:
 
-                                categoriaDAO.listar().forEach(c ->
-                                        System.out.println(
+                                categoriaDAO.listar().forEach(c
+                                        -> System.out.println(
                                                 "ID: " + c.getId()
                                                 + " | Nome: " + c.getNome()
                                         )
@@ -124,8 +130,8 @@ public class main {
 
                             case 1:
 
-                                clienteDAO.listar().forEach(c ->
-                                        System.out.println(
+                                clienteDAO.listar().forEach(c
+                                        -> System.out.println(
                                                 "ID: " + c.getId()
                                                 + " | Nome: " + c.getNome()
                                                 + " | Cidade: " + c.getCidade()
@@ -208,6 +214,207 @@ public class main {
                         }
 
                     } while (opCliente != 0);
+
+                    break;
+
+                case 3:
+
+                    int opContas;
+
+                    do {
+
+                        System.out.println("\n=== MENU CONTAS A PAGAR ===");
+                        System.out.println("1 - Listar");
+                        System.out.println("2 - Inserir");
+                        System.out.println("3 - Atualizar");
+                        System.out.println("4 - Deletar");
+                        System.out.println("0 - Voltar");
+                        System.out.print("Opção: ");
+
+                        opContas = sc.nextInt();
+                        sc.nextLine();
+
+                        switch (opContas) {
+
+                            case 1:
+
+                                contasPagarDAO.listar().forEach(cp
+                                        -> System.out.println(
+                                                "ID: " + cp.getId()
+                                                + " | Valor: " + cp.getValor()
+                                                + " | Data: " + cp.getData()
+                                                + " | Status: " + cp.getStatus()
+                                                + " | ID Fornecedor: " + cp.getId_fornecedor()
+                                        )
+                                );
+
+                                break;
+
+                            case 2:
+
+                                ContasPagar novaConta = new ContasPagar();
+
+                                System.out.print("Valor: ");
+                                novaConta.setValor(sc.nextFloat());
+                                sc.nextLine();
+
+                                System.out.print("Data: ");
+                                novaConta.setData(sc.nextLine());
+
+                                System.out.print("Status: ");
+                                novaConta.setStatus(sc.nextLine());
+
+                                System.out.print("ID do fornecedor: ");
+                                novaConta.setId_fornecedor(sc.nextInt());
+                                sc.nextLine();
+
+                                contasPagarDAO.inserir(novaConta);
+
+                                System.out.println("Conta cadastrada!");
+                                break;
+
+                            case 3:
+
+                                ContasPagar editarConta = new ContasPagar();
+
+                                System.out.print("ID da conta: ");
+                                editarConta.setId(sc.nextInt());
+                                sc.nextLine();
+
+                                System.out.print("Valor: ");
+                                editarConta.setValor(sc.nextFloat());
+                                sc.nextLine();
+
+                                System.out.print("Data: ");
+                                editarConta.setData(sc.nextLine());
+
+                                System.out.print("Status: ");
+                                editarConta.setStatus(sc.nextLine());
+
+                                System.out.print("ID do fornecedor: ");
+                                editarConta.setId_fornecedor(sc.nextInt());
+                                sc.nextLine();
+
+                                contasPagarDAO.atualizar(editarConta);
+
+                                System.out.println("Conta atualizada!");
+                                break;
+
+                            case 4:
+
+                                System.out.print("ID da conta: ");
+                                int idConta = sc.nextInt();
+
+                                contasPagarDAO.deletar(idConta);
+
+                                System.out.println("Conta removida!");
+                                break;
+
+                            case 0:
+                                break;
+
+                            default:
+                                System.out.println("Opção inválida!");
+                        }
+
+                    } while (opContas != 0);
+
+                    break;
+
+                case 4:
+
+                    int opReceber;
+
+                    do {
+
+                        System.out.println("\n=== MENU CONTAS A RECEBER ===");
+                        System.out.println("1 - Listar");
+                        System.out.println("2 - Inserir");
+                        System.out.println("3 - Atualizar");
+                        System.out.println("4 - Deletar");
+                        System.out.println("0 - Voltar");
+                        System.out.print("Opção: ");
+
+                        opReceber = sc.nextInt();
+                        sc.nextLine();
+
+                        switch (opReceber) {
+
+                            case 1:
+
+                                contasReceberDAO.listar().forEach(cr
+                                        -> System.out.println(
+                                                "ID: " + cr.getId()
+                                                + " | Valor: " + cr.getValor()
+                                                + " | Data Vencimento: " + cr.getData_vencimento()
+                                                + " | Item Venda ID: " + cr.getId_item_venda()
+                                        )
+                                );
+
+                                break;
+
+                            case 2:
+
+                                ContasReceber novaReceber = new ContasReceber();
+
+                                System.out.print("Valor: ");
+                                novaReceber.setValor(sc.nextFloat());
+                                sc.nextLine();
+
+                                System.out.print("Data de vencimento: ");
+                                novaReceber.setData_vencimento(sc.nextLine());
+
+                                System.out.print("ID Item Venda: ");
+                                novaReceber.setId_item_venda(sc.nextInt());
+                                sc.nextLine();
+
+                                contasReceberDAO.inserir(novaReceber);
+
+                                System.out.println("Conta cadastrada!");
+                                break;
+
+                            case 3:
+
+                                ContasReceber editarReceber = new ContasReceber();
+
+                                System.out.print("ID: ");
+                                editarReceber.setId(sc.nextInt());
+                                sc.nextLine();
+
+                                System.out.print("Valor: ");
+                                editarReceber.setValor(sc.nextFloat());
+                                sc.nextLine();
+
+                                System.out.print("Data de vencimento: ");
+                                editarReceber.setData_vencimento(sc.nextLine());
+
+                                System.out.print("ID Item Venda: ");
+                                editarReceber.setId_item_venda(sc.nextInt());
+                                sc.nextLine();
+
+                                contasReceberDAO.atualizar(editarReceber);
+
+                                System.out.println("Conta atualizada!");
+                                break;
+
+                            case 4:
+
+                                System.out.print("ID da conta: ");
+                                int idReceber = sc.nextInt();
+
+                                contasReceberDAO.deletar(idReceber);
+
+                                System.out.println("Conta removida!");
+                                break;
+
+                            case 0:
+                                break;
+
+                            default:
+                                System.out.println("Opção inválida!");
+                        }
+
+                    } while (opReceber != 0);
 
                     break;
 
